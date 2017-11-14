2277cff451e2f43a043f9991c2570da30ca54750
Admin <h1410115@nushigh.edu.sg>
2017-11-14 11:00:28 +0800

    Finished TODO Board

--- app/src/main/java/com/sccl/nikonikonii/sccl/MainActivity.java
+++ app/src/main/java/com/sccl/nikonikonii/sccl/MainActivity.java
@@ -9,5 +9,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //todos in order of how user will see when they enter the game
+        //TODO Log in screen
+        //TODO Player Data Structure
+        //TODO Choose level of education
+        //TODO Learning phase
+            //Phase 1
+            //Upper Levels -> Flashcard w/ HYPY + Relevant pic
+            //Lower Levels -> Jigsaw puzzle showing food/occupation
+            //Phase 2
+            //Character writing
+        //TODO Practising phase
+            //Racing to get the words and shit
+        //TODO Test phase
+            //Simple sentence structure
+
+        //TODO Sound effects
+        //TODO mascot to give congratulation message/ try harder message or smthing
+        //TODO Leveling & Achievement system + Badges
+        //TODO Customization
+        //Harder shit
+        //TODO wifi and shit to share profile
+        //TODO find something to make them keep playing bro
     }
 }
