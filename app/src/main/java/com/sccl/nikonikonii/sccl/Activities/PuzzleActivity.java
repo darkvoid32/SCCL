9cc666e97b34960b82eb9cdd92dcd6fdf0044339
Cxo05 <xavieroh05@gmail.com>
2017-11-15 13:30:37 +0800

    Image Splitting

--- app/src/main/java/com/sccl/nikonikonii/sccl/Activities/PuzzleActivity.java
+++ app/src/main/java/com/sccl/nikonikonii/sccl/Activities/PuzzleActivity.java
@@ -1,14 +1,24 @@
 package com.sccl.nikonikonii.sccl.Activities;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TableRow;
 
 import com.sccl.nikonikonii.sccl.R;
+import com.sccl.nikonikonii.sccl.Utils.ImagePiece;
+import com.sccl.nikonikonii.sccl.Utils.ImageUtil;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class PuzzleActivity extends AppCompatActivity{
 
@@ -30,5 +40,34 @@ public class PuzzleActivity extends AppCompatActivity{
                 asd.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
             }
         });
+
+        ImageView puzzleInit = findViewById(R.id.puzzleInit);
+        puzzleInit.setVisibility(View.INVISIBLE);
+
+        ImageUtil imgUtil = new ImageUtil();
+
+        ArrayList<Bitmap> bitmapPieces = imgUtil.splitImage(puzzleInit);
+        ArrayList<ImagePiece> imagePieces = new ArrayList<ImagePiece>(12);
+
+        for(int i = 0; i < bitmapPieces.size();i++) {
+            ImagePiece piece = new ImagePiece(getApplicationContext(), bitmapPieces.get(i), i);
+            imagePieces.add(piece);
+        }
+
+        TableRow row1 = findViewById(R.id.firstRow);
+        TableRow row2 = findViewById(R.id.secondRow);
+        TableRow row3 = findViewById(R.id.thirdRow);
+
+        for(int i = 0;i < imagePieces.size();i++){
+            if(i <= imagePieces.size()/3){
+                row1.addView(imagePieces.get(i));
+            }else if(i <= (imagePieces.size()/3)*2){
+                row2.addView(imagePieces.get(i));
+            }else{
+                row3.addView(imagePieces.get(i));
+            }
+        }
     }
+
+
 }
