473504a31794092f2326b53dd904fa06245d4818
Admin <h1410115@nushigh.edu.sg>
2017-11-15 11:04:21 +0800

    Added gitignore

--- app/src/main/java/com/sccl/nikonikonii/sccl/Activities/PuzzleActivity.java
+++ app/src/main/java/com/sccl/nikonikonii/sccl/Activities/PuzzleActivity.java
@@ -2,14 +2,26 @@ package com.sccl.nikonikonii.sccl.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.sccl.nikonikonii.sccl.R;
+import com.sccl.nikonikonii.sccl.Utils.CanvasView;
 
 public class PuzzleActivity extends AppCompatActivity{
+
+    private CanvasView customCanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
+
+        customCanvas = (CanvasView) findViewById(R.id.canvasView);
+
     }
+
+    public void clearCanvas(View v) {
+        customCanvas.clearCanvas();
+    }
+
 }
